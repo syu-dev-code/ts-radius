bf79400b7e4d0d103caab85eec3957afdfb3fa39
syu_dev <136579726+syu-dev-code@users.noreply.github.com>
2025-02-27 23:26:03 +0900

    Remove unused module

--- src/protocol/packet/Identifier.ts
+++ src/protocol/packet/Identifier.ts
@@ -1,16 +1,9 @@
 import type { RemoteInfo } from 'dgram';
 
-export class Identifier {
-  private provider: IIdentifierProvider;
-  constructor(provider: IIdentifierProvider) {
-    this.provider = provider;
-  }
-}
-
 export class MemoryIdentifierProvider implements IIdentifierProvider {
   private store: Map<string, Map<number, Date>> = new Map();
 
-  generateKey(rinfo: RemoteInfo): string {
+  private generateKey(rinfo: RemoteInfo): string {
     return `${rinfo.address}:${rinfo.port}`;
   }
 
@@ -39,17 +32,11 @@ export class MemoryIdentifierProvider implements IIdentifierProvider {
     const key = this.generateKey(rinfo);
     this.store.delete(key);
   }
-
-  getAll(): { id: number; createdAt: Date }[] {
-    return Array.from(this.store.values())
-      .flatMap((ids) => Array.from(ids.entries()))
-      .map(([id, createdAt]) => ({ id, createdAt }));
-  }
 }
 
 interface IIdentifierProvider {
-  has(rinfo: RemoteInfo, id: number): boolean;
   set(rinfo: RemoteInfo, id: number): number;
+  has(rinfo: RemoteInfo, id: number): boolean;
   remove(rinfo: RemoteInfo, id: number): void;
   clear(rinfo: RemoteInfo): void;
 }
