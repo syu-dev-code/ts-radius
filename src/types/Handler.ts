92cd29f15d290cfda6246d802f3bb697a14d6c1b
syu_dev <136579726+syu-dev-code@users.noreply.github.com>
2025-03-15 20:40:36 +0900

    Remove unused type

--- src/types/Handler.ts
+++
@@ -1,7 +0,0 @@
-import type { RemoteInfo } from 'dgram';
-
-export type PacketHandler = (packet: Buffer, rinfo: RemoteInfo) => Promise<PacketHandlerResult>;
-export type PacketHandlerResult = {
-  doNext: boolean;
-  response: Buffer | false;
-};
