c83c8feffdeb858a1f3162818f9a9b0b033430ee
syu_dev <136579726+syu-dev-code@users.noreply.github.com>
2025-04-12 00:22:48 +0900

    Prioritize logger initialization

--- tools/Radius.ts
+++ tools/Radius.ts
@@ -5,6 +5,8 @@ import { MemoryRadiusTransaction } from '@app/protocol/packet/RadiusTransaction'
 import { DefaultNasProvider } from '@app/protocol/nas/DefaultNasProvider';
 import { StdOutLogger } from '@app/logger/StdOutLogger';
 import { Logger } from '@app/logger/Logger';
+
+Logger.setDelegate(new StdOutLogger());
 const nasProvider = new DefaultNasProvider([
   {
     shortName: 'test',
@@ -18,6 +20,5 @@ const nasProvider = new DefaultNasProvider([
 const transaction = new MemoryRadiusTransaction();
 const radiusPacketHandler = new RadiusPacketHandler(nasProvider, transaction);
 const concurrentPacketHandler = new ConcurrentPacketHandler(100, radiusPacketHandler);
-Logger.setDelegate(new StdOutLogger());
 const server = new UDPServer(1812, '127.0.0.1', concurrentPacketHandler);
 await server.start();
